325016be9bec73a93e9f5b9178b9a03e658bf6ad
Kevin Hernandez <kmhs-18@hotmail.com>
2023-03-19 15:21:09 -0300

    Initial Commit

---
+++ 03-1-days--game-rock-paper-scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  03-1-days--game-rock-paper-scissors
+//
+//  Created by Kevin Hernandez on 19/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 03-1-days--game-rock-paper-scissors/_3_1_days__game_rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_1_days__game_rock_paper_scissorsApp.swift
+//  03-1-days--game-rock-paper-scissors
+//
+//  Created by Kevin Hernandez on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct _3_1_days__game_rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
